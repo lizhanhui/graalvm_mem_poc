481036ae97ec7f5715c1e0f592ef6b57f6790665
shutian.lzh <shutian.lzh@alibaba-inc.com>
2019-05-17 19:41:13 +0800

    extern C for struct

--- client/src/main/c/simple_data.h
+++ client/src/main/c/simple_data.h
@@ -1,9 +1,17 @@
 ##ifndef _DATA_H_
 ##define _DATA_H_
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 typedef struct simple_data_struct {
     char *str;
     int len;
 } simple_data;
+
+#ifdef __cplusplus
+}
+#endif
 
 ##endif
