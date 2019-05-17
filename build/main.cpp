8a7cf0cb2fa1fca88226a5015286d30578518d7a
shutian.lzh <shutian.lzh@alibaba-inc.com>
2019-05-17 21:10:08 +0800

    Add main.cpp

--- build/main.cpp
+++ build/main.cpp
@@ -5,7 +5,33 @@
 #include "poc/data.h"
 
 int main(int argc, char **argv) {
-  
+    graal_isolate_t *isolate = nullptr;
+    graal_isolatethread_t *thread = nullptr;
 
+    if (graal_create_isolate(nullptr, &isolate, &thread)) {
+        std::cout << "Failed to create isolate and thread" << std::endl;
+        return -1;
+    }
 
+    int instance_id = create_instance(thread);
+
+    int data_len = 1024;
+    char data[data_len + 1];
+    for (int i = 0; i < data_len; i++) {
+        data[i] = 'x';
+    }
+    data[data_len] = '\0';
+
+    my_data content = {
+        .f_str = data,
+        .len = data_len
+    };
+
+    while(true) {
+        if (ping(thread, instance_id, &content)) {
+            std::cout << "Ping failed" << std::endl;
+        }
+    }
+
+    return 0;
 }
