24055059bbe1d93ccc7d8512e9f125c1cdd1bd95
shutian.lzh <shutian.lzh@alibaba-inc.com>
2019-05-17 21:59:04 +0800

    Add script to generate reflection file

---
+++ gen.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+java -agentlib:native-image-agent=trace-output=./trace-file.json -jar client/target/client.jar
+./native-image-configure process-trace --output-dir=. ./trace-file.json
